d24e5cd700f4edb9e22450a31cc99a0e746bb608
Ashley <ashtay@MSI.localdomain>
2024-03-31 20:59:52 -0700

    Added function prototypes for options

--- main.c
+++ main.c
@@ -7,7 +7,24 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
 
-    printf("Hello world\n%d%s", argc, argv[0]);
+    //printf("Hello world\n%d%s", argc, argv[0]);
+
+    if (my_strcmp("-c", argv[1]) {
+	//create new archive containing the specified items
+    }
+    else if (my_strcmp("-r", argv[1]) {
+	//like -c but new entries are appended to the archive. the -f option is req"
+    }
+    else if (my_strcmp("-t", argv[1]) {
+	//list archive contents to stdout
+    }
+    else if (my_strcmp("-u", argv[1]) {
+	//like -r, but new entries are added only if they have a modification date newer than the corresponding entry in the archive. the -f option is req
+    }
+    else if (my_strcmp("-x", argv[1]) {
+	//extract to disk from the archive. if a file with the smae name appears more than once in the archive, each copy will be extracted, with later copies overwriting (replacing) earlier copies
+    }
+
 
     return 0;
 }
--- my_tar.h
+++ my_tar.h
@@ -12,5 +12,10 @@ void print_message(char* message);
 int my_strlen(const char* str_1);
 char* my_strdup(const char* str_1);
 int my_strcmp(const char* str_1, const char* str_2);
+void create_archive(int argc, char* argv);
+void append_archive(int argc, char* argv);
+void list_archive(char* archive_name);
+void update_archive(int argc, char* argv);
+void extract_archive(char* archive_name);
 
 #endif
