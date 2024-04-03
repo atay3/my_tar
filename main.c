0dc969dc8f8b2e6a5728368664b556d7d48424d1
Ashley <ashtay@MSI.localdomain>
2024-04-02 23:43:58 -0700

    Completed the feature that creates a new archive and writes the metadata into the archive. May need more testing.

--- main.c
+++ main.c
@@ -31,5 +31,57 @@ int main(int argc, char** argv) {
 void create_archive(int argc, char** argv) {
     char* archive_name = argv[2];
 
-    int file_descriptor = open(archive_name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    int archive_file_des = open(archive_name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    for (int i = 3; i < argc; i++) {
+        write_metadata(archive_file_des, argv[i]);
+        write_file_content(archive_file_des, argv[i]);
+    }
+
+    close(archive_file_des);
+}
+
+void write_metadata(int archive_file_des, const char* file_name) {
+    struct stat file_stat;
+
+    if (lstat(file_name, &file_stat) == -1) {
+        printf("Error retrieving file stats\n"); //remove later
+        return;
+    }
+
+    struct FileMetadata metadata;
+
+    metadata.mode = file_stat.st_mode;
+    metadata.uid = file_stat.st_uid;
+    metadata.gid = file_stat.st_gid;
+    metadata.size = file_stat.st_size;
+    metadata.time = file_stat.st_mtim;
+
+    write(archive_file_des, file_name, my_strlen(file_name));
+
+    if (write(archive_file_des, &metadata, sizeof(struct FileMetadata)) != sizeof(struct FileMetadata)) {
+        printf("Error writing file stats\n"); //remove later
+        return;
+    }
+}
+
+void write_file_content(int archive_file_des, const char* file_name) {
+    int file_fd = open(file_name, O_RDONLY);
+
+    if (file_fd == -1) {
+        printf("Error opening file\n"); //remove later
+        return;
+    }
+
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
+
+    while ((bytes_read = read(file_fd, buffer, BUFFER_SIZE)) > 0) {
+        if (write(archive_file_des, buffer, bytes_read) != bytes_read) {
+            printf("Error writing file contents\n"); //remove later
+            return;
+        }
+    }
+
+    close(file_fd);
 }
--- my_tar.h
+++ my_tar.h
@@ -4,10 +4,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 
 #include <stdio.h>
 
 #define ERROR_MSG "my_tar: Insufficient number of arguments\n"
+#define BUFFER_SIZE 1024
+
+struct FileMetadata {
+    char* name;
+    size_t name_length;
+    size_t size;
+    struct timespec time; //modification time
+    mode_t mode; //permissions
+    uid_t uid; //user id
+    gid_t gid; //group id
+};
 
 void print_message(char* message);
 void print_error(char* message);
@@ -15,6 +27,8 @@ int my_strlen(const char* str_1);
 char* my_strdup(const char* str_1);
 int my_strcmp(const char* str_1, const char* str_2);
 void create_archive(int argc, char** argv);
+void write_metadata(int archive_file_des, const char* file_name);
+void write_file_content(int archive_file_des, const char* file_name);
 void append_archive(int argc, char** argv);
 void list_archive(char* archive_name);
 void update_archive(int argc, char** argv);
